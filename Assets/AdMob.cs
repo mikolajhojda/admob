cef8cdca6a1eea8df7915441d887ff30ee682615
Mikołaj Hojda <32071801+mikolajhojda@users.noreply.github.com>
2020-06-13 13:17:12 +0200

    admob

---
+++ Assets/AdMob.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleMobileAds.Api;
+using System;
+
+public class AdMob : MonoBehaviour
+{
+    public Text addStatus;
+
+    string App_ID = "ca-app-pub-8701386067962256~4637105317";
+
+    string Banner_Ad_ID = "ca-app-pub-3940256099942544/6300978111";
+    string Interstitial_Ad_ID = "ca-app-pub-3940256099942544/1033173712";
+
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+
+    void Start()
+    {
+        MobileAds.Initialize(App_ID);
+    }
+
+    public void RequestBanner()
+    {
+        bannerView = new BannerView(Banner_Ad_ID, AdSize.Banner, AdPosition.Bottom);
+
+        // Called when an ad request has successfully loaded.
+        this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        this.bannerView.OnAdOpening += this.HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        this.bannerView.OnAdClosed += this.HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
+    }
+
+    public void ShowBannerAd()
+    {
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        addStatus.text = "Add loaded";
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        addStatus.text = "Add failed loaded";
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+}
